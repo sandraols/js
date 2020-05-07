267c567e49ec4ec9e64c8f250bd209be0bc0d3fc
sandraols <sandra.helene.olsson@gmail.com>
2020-05-07 10:21:58 +0200

    add slider-page

---
+++ slider/assets/script.js
@@ -0,0 +1,24 @@
+$('.kitty-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+});
+
+$('.snake-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+});
+
+$('.raccoon-rollercoaster').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
